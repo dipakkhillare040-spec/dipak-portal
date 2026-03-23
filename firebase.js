a062ca551837b96581b013a371625040462ff01e
Dipak Khillare <dipakkhillare040@gmail.com>
2026-03-23 15:51:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyC0ovsy4z24065EAWijE5rIP1roTjh9eLE",
   authDomain: "dipak-portal.firebaseapp.com",
@@ -8,5 +7,4 @@ const firebaseConfig = {
   appId: "1:395089848028:web:fe3267620c3bfabfae7619"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
