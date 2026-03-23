4b924f98bffacd96401aad2967a89d795f44b35c
Dipak Khillare <dipakkhillare040@gmail.com>
2026-03-23 15:40:40 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
-// Firebase config paste karo yaha
+// Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID",
+  apiKey: "AIzaSyC0ovsy4z24065EAWijE5rIP1roTjh9eLE",
+  authDomain: "dipak-portal.firebaseapp.com",
+  projectId: "dipak-portal",
+  storageBucket: "dipak-portal.appspot.com",
+  messagingSenderId: "395089848028",
+  appId: "1:395089848028:web:fe3267620c3bfabfae7619"
 };
 
 // Initialize Firebase
