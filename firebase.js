c2d57c6222afee11d9b5dee6a58346d0d73bf47e
Dipak Khillare <dipakkhillare040@gmail.com>
2026-03-23 17:09:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,10 @@
-// Firebase SDK import
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// 🔥 YOUR REAL CONFIG (copy from Firebase)
 const firebaseConfig = {
-  apiKey: "AIzaSyCo7ovs4z24O65EAWijE5rIP1roTjh9el",
+  apiKey: "AIzaSyC0vovs4z24065EAWiEj5ErIP1roTjh9e",
   authDomain: "dipak-portal.firebaseapp.com",
   projectId: "dipak-portal",
-  storageBucket: "dipak-portal.firebasestorage.app",
+  storageBucket: "dipak-portal.appspot.com",
   messagingSenderId: "395089848028",
   appId: "1:395089848028:web:fe3267620c3bfabfae7619"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Export
-export { auth };
+firebase.initializeApp(firebaseConfig);
