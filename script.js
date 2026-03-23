2982836eeab4ad95eaa29ac0eaed6ac4cfbed53a
Dipak Khillare <dipakkhillare040@gmail.com>
2026-03-23 16:47:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 function login() {
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => {
-      document.getElementById("msg").innerText = "Login Success ✅";
-      window.location.href = "index.html";
-    })
-    .catch((error) => {
-      document.getElementById("msg").innerText = error.message;
-    });
+  alert("Button Working 🔥");
 }
